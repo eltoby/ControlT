aa820f20f4987122a6cc0668ec426a0b0c392cf8
Pablo Rausch <pablorausch@gmail.com>
2014-12-21 15:56:48 -0300

    Refactor

---
+++ ControlT/ControlT.Utils/StringExtensionMethods.cs
@@ -0,0 +1,13 @@
+﻿namespace ControlT.Utils
+{
+    using System;
+
+    public static class StringExtensionMethods
+    {
+        public static DateTime ToDate(this string texto)
+        {
+            var partes = texto.Split('-');
+            return new DateTime(int.Parse(partes[2]), int.Parse(partes[1]), int.Parse(partes[0]));
+        }
+    }
+}
