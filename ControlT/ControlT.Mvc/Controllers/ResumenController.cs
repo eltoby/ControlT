2f53ad5a60a705a15a4a1d56d4397f238a3d576b
Pablo Rausch <pablorausch@gmail.com>
2015-05-03 03:44:59 -0300

    inicio de nueva pantalla de resumen

---
+++ ControlT/ControlT.Mvc/Controllers/ResumenController.cs
@@ -0,0 +1,21 @@
+﻿namespace ControlT.Mvc.Controllers
+{
+    using System.Linq;
+    using System.Web.Mvc;
+    using System.Web.Services.Protocols;
+    using EF;
+    using Models;
+
+    public class ResumenController : Controller
+    {
+
+        public ActionResult Index()
+        {
+            var uow = new ControlTContext();
+            var movimientos = uow.Movimientos.ToArray();
+            var model = new ResumenModel(movimientos);
+            return View(model);
+        }
+
+    }
+}
---
+++ ControlT/ControlT.Mvc/Models/ResumenModel.cs
@@ -0,0 +1,13 @@
+﻿namespace ControlT.Mvc.Models
+{
+    using System.Collections.Generic;
+    using EF;
+
+    public class ResumenModel
+    {
+        public ResumenModel(IEnumerable<Movimiento> movimientos)
+        {
+            
+        }
+    }
+}
